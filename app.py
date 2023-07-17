7a0b8b9e127f352bb1a4285838f6b12043a042c4
EunAh <eunahp86@gmail.com>
2023-07-17 12:27:47 +0900

    POST,GET Step 1

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+       os_info = dict(request.headers)
+       print(os_info)
+       return render_template('index.html',header=os_info)
+    elif request.method == "POST":
+       return render_template('index.html',header="안녕하세요, 배은아입니다.")
+        
+    # req_data = request.data
+    # print(req_data)
+   
+    #  render_template('index.html',jinja2문법(가변키워드인자로 전달받음) 파이썬언어는 {%~~~~%}, 전달할 데이터 직접전달시 {{~~~~}}  )
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
+    # debug=True 파일을 수정해도 재실행안해도 업데이트 되는 코드
